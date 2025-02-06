bf935b374393a7ddc9e5650016a53b991f585a9a
Rachel Yelloz <rsy262618@gmail.com>
2025-02-06 18:39:55 +0200

    hh

--- Program.cs
+++ Program.cs
@@ -1,5 +1,74 @@
 
 
+// using Microsoft.EntityFrameworkCore;
+// using TodoApi;
+
+// var builder = WebApplication.CreateBuilder(args);
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("AllowAllOrigins",
+//         builder => builder.AllowAnyOrigin()
+//                           .AllowAnyMethod()
+//                           .AllowAnyHeader());
+// });
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+// var connectionString = builder.Configuration.GetConnectionString("ToDoDB");
+// builder.Services.AddDbContext<ToDoDbContext>(options =>
+//     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+// );
+
+// var app = builder.Build();
+// app.UseCors("AllowAllOrigins");
+
+
+// app.MapGet("/items", async (ToDoDbContext db) => { return await db.Items.ToListAsync(); });
+
+// app.MapPost("items/", (Item item, ToDoDbContext db) => 
+// {
+//     db.Items.Add(item);
+//     db.SaveChanges(); 
+//     return Results.Created($"/{item.Id}", item); 
+// });
+
+// app.MapPut("/items/{id}", (int id, Item updatedItem, ToDoDbContext db) => 
+// {
+//     var item = db.Items.Find(id);
+//     if (item is null) return Results.NotFound();
+    
+//     // עדכון רק את הסטטוס אם הוא נשלח
+//     if (updatedItem.IsComplete.HasValue)
+//     {
+//         item.IsComplete = updatedItem.IsComplete;
+//     }
+//     if (!string.IsNullOrEmpty(updatedItem.Name))
+//     {
+//         item.Name = updatedItem.Name;
+//     }
+    
+//     db.SaveChanges();
+//     return Results.NoContent();
+// });
+
+// app.MapDelete("/items/{id}", (int id, ToDoDbContext db) => 
+// {
+//     var item = db.Items.Find(id);
+//     if (item is null) return Results.NotFound();
+    
+//     db.Items.Remove(item);
+//     db.SaveChanges();
+//     return Results.NoContent();
+// });
+
+// app.UseSwagger();  
+// app.UseSwaggerUI(c =>
+// {
+//     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+//     c.RoutePrefix = string.Empty;
+// });
+
+// app.MapGet("/",()=>"Authserver API is running");
+// app.Run();
 using Microsoft.EntityFrameworkCore;
 using TodoApi;
 
@@ -13,7 +82,6 @@ builder.Services.AddCors(options =>
 });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// builder.Services.AddDbContext<ToDoDbContext>(); // שימוש במסד נתונים בזיכרון
 var connectionString = builder.Configuration.GetConnectionString("ToDoDB");
 builder.Services.AddDbContext<ToDoDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
@@ -22,18 +90,9 @@ builder.Services.AddDbContext<ToDoDbContext>(options =>
 var app = builder.Build();
 app.UseCors("AllowAllOrigins");
 
-
-//app.MapGet("/items", (ToDoDbContext db) => db.Items.ToList());
 app.MapGet("/items", async (ToDoDbContext db) => { return await db.Items.ToListAsync(); });
 
-// app.MapPost("/items", (string name, ToDoDbContext db) => 
-// {
-//     Item item=new Item(){Name=name,IsComplete=false};
-//     db.Items.Add(item);
-//     db.SaveChanges();
-//     return Results.Created($"/items/{item.Id}", item);
-// });
-app.MapPost("items/", (Item item, ToDoDbContext db) => 
+app.MapPost("/items/", (Item item, ToDoDbContext db) => 
 {
     db.Items.Add(item);
     db.SaveChanges(); 
@@ -68,11 +127,7 @@ app.MapDelete("/items/{id}", (int id, ToDoDbContext db) =>
     db.SaveChanges();
     return Results.NoContent();
 });
-// //if (app.Environment.IsDevelopment())
-// //{
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// //}
+
 app.UseSwagger();  
 app.UseSwaggerUI(c =>
 {
--- ToDoDbContext.cs
+++ ToDoDbContext.cs
@@ -1,4 +1,50 @@
-﻿using System;
+﻿// using System;
+// using System.Collections.Generic;
+// using Microsoft.EntityFrameworkCore;
+// using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+// namespace TodoApi;
+
+// public partial class ToDoDbContext : DbContext
+// {
+//     public ToDoDbContext()
+//     {
+//     }
+
+//     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+//         : base(options)
+//     {
+//     }
+
+//     public virtual DbSet<Item> Items { get; set; }
+
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+//         var connectionString= "server=bqlfth4bplu23mk1oxkl-mysql.services.clever-cloud.com;user=ufsi6klspjldeafu;password=lhIdQGiAtQVhKfeWBLOq;database=bqlfth4bplu23mk1oxkl";
+//          optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql"));
+
+//     }
+
+//     protected override void OnModelCreating(ModelBuilder modelBuilder)
+//     {
+//         modelBuilder
+//             .UseCollation("utf8mb4_0900_ai_ci")
+//             .HasCharSet("utf8mb4");
+
+//         modelBuilder.Entity<Item>(entity =>
+//         {
+//             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+//             entity.ToTable("items");
+
+//             entity.Property(e => e.Name).HasMaxLength(100);
+//         });
+
+//         OnModelCreatingPartial(modelBuilder);
+//     }
+
+//     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+// }
+using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
@@ -16,12 +62,12 @@ public partial class ToDoDbContext : DbContext
     {
     }
 
-    public virtual DbSet<Item> Items { get; set; }
+    public virtual DbSet<Item> Items { get; set; } // השאר את זה באות גדולה
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
-        var connectionString= "server=bqlfth4bplu23mk1oxkl-mysql.services.clever-cloud.com;user=ufsi6klspjldeafu;password=lhIdQGiAtQVhKfeWBLOq;database=bqlfth4bplu23mk1oxkl";
-         optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql"));
-
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "server=bqlfth4bplu23mk1oxkl-mysql.services.clever-cloud.com;user=ufsi6klspjldeafu;password=lhIdQGiAtQVhKfeWBLOq;database=bqlfth4bplu23mk1oxkl";
+        optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql"));
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -34,7 +80,7 @@ public partial class ToDoDbContext : DbContext
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
 
-            entity.ToTable("items");
+            entity.ToTable("Items"); // עדכן את שם הטבלה לאות גדולה
 
             entity.Property(e => e.Name).HasMaxLength(100);
         });
